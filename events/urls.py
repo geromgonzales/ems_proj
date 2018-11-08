0df9db9eb510820f9023089bd5d7631e43f38322
geromgonzales <gonzalesgerom23@gmail.com>
2018-11-08 10:42:33 +0800

    implemented MyEvents,Create and update Events

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import HomePageView,EventListView,EventDetailView
+from .views import EventCreateView,MyEventListView, HomePageView,EventListView,EventDetailView,EventUpdateView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name = "home"),
     path("events/", EventListView.as_view(), name = "event_list"),
+    path("events/myevents/", MyEventListView.as_view(), name = "myevent_list"),
+    path("events/addevent/", EventCreateView.as_view(), name = "event_create"),
+    path("events/<int:pk>/updateevent/", EventUpdateView.as_view(), name = "event_update"),
     path("events/<int:pk>/", EventDetailView.as_view(), name = "event_detail")
+
 
 ]
