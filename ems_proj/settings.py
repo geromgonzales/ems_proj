79b89deb7fae9c98aa64465e9d7cb55aef8b04d3
geromgonzales <gonzalesgerom23@gmail.com>
2018-11-08 12:03:47 +0800

    implemented

--- ems_proj/settings.py
+++ ems_proj/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'events',
     'crispy_forms',
-    'users'
+    'users',
+    'registrations'
+
 ]
 
 MIDDLEWARE = [
--- ems_proj/urls.py
+++ ems_proj/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("django.contrib.auth.urls")),
     path('users/', include("users.urls")),
-    path('', include("events.urls"))
+    path('', include("events.urls")),
+    path('', include("registrations.urls"))
     
 ]
